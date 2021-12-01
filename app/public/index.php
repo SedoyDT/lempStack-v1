e490480560830e4d022fa8dd4d206833bd4a1a34
Anatoliy <mazzzfaka@gmail.com>
2021-12-01 17:29:07 +0300

    переместил public в папку app

--- app/public/index.php
+++ app/public/index.php
@@ -1,6 +1,5 @@
 <?php
-echo "Hello from PHP inside nginx container";
-//d
-//fs
+echo "1 from PHP inside nginx container";
+
 // вот таким образом контейнер php будет обращаться к контейнеру mysql
 //host: mysql:3306
